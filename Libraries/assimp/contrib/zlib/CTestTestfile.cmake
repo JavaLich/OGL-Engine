5745f0681e7f6bbd1233e0907964b5cbe0c9959c
javal <javal@LAPTOP-GAD0I6UM>
2019-04-14 14:46:50 -0500

    Project settings update

---
+++ Libraries/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Akash/Desktop/Akash/C++/Workspace/OGL/Libraries/assimp-4.0.1/contrib/zlib
+# Build directory: C:/Users/Akash/Desktop/Akash/C++/Workspace/OGL/Libraries/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Libraries/assimp/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Akash/Desktop/Akash/C++/Workspace/OGL/Libraries/assimp-4.0.1/test
+# Build directory: C:/Users/Akash/Desktop/Akash/C++/Workspace/OGL/Libraries/assimp/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittests "unit")
+subdirs(headercheck)
